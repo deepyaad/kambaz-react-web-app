e6ab00a16498aff874d1009b01e3f7d4061fa33e
anissavaughn <anissa.vaughnn@gmail.com>
2025-06-17 23:14:19 -0400

    fixing deployment issue

--- src/kambaz/account/client.ts
+++ src/kambaz/account/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+console.log("React app's API base URL:", REMOTE_SERVER);
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
