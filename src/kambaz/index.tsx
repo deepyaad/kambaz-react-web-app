8fc2031f14b778cadd6c14ddd7f2d7819ee7de6c
anissavaughn <anissa.vaughnn@gmail.com>
2025-06-18 00:24:39 -0400

    delete courses

--- src/kambaz/index.tsx
+++ src/kambaz/index.tsx
@@ -38,7 +38,7 @@ export default function Kambaz() {
   }, [currentUser]);
 
   const deleteCourse = async (courseId: string) => {
-    // const status = await courseClient.deleteCourse(courseId);
+    await courseClient.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
 
